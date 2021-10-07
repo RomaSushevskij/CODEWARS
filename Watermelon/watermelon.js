2756ea4ce534194fe2f7cb465243f9f76b66909e
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-10-07 20:50:38 +0300

    watermelon_solved

---
+++ Watermelon/watermelon.js
@@ -0,0 +1,6 @@
+function divide(weight){
+    if (weight >=1 && weight <= 100 && Number.isInteger(weight)) {
+        return (Number.isInteger(weight / 2) && weight/2 > 1);
+
+    } return false;
+}
