0f9369cfcffced43df3941f111640cb8a03a3b8b
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-10-19 19:49:45 +0300

    Convert boolean values to strings 'Yes' or 'No'.

--- Sum of positive/solve.js
+++ Sum of positive/solve.js
@@ -1,4 +1,4 @@
 function positiveSum(arr) {
-    return arr.filter(el => el>=0).reduce((init, current)=> init+current, 0)
+    return arr.filter(el => el >= 0).reduce((init, current) => init + current, 0)
 
 }
