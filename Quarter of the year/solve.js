328f840a4947518ecbddd37bb4afead1f7aedaf9
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-10-27 13:27:38 +0300

    Quarter of the year

---
+++ Quarter of the year/solve.js
@@ -0,0 +1,25 @@
+const quarterOf1 = (month) => {
+    switch (month) {
+        case 1:
+        case 2:
+        case 3:
+            return 1
+        case 4:
+        case 5:
+        case 6:
+            return 2
+        case 7:
+        case 8:
+        case 9:
+            return 3
+        case 10:
+        case 11:
+        case 12:
+            return 4
+    }
+}
+const quarterOf = (month) => {
+    return (month > 0 && month < 4) ? 1 :
+        (month > 3 && month < 7) ? 2 :
+            (month > 6 && month < 10) ? 3 : 4
+}
