fa560a734ce30a19789a0296df37822c4cfc081f
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-10-28 13:17:49 +0300

    The Wide-Mouthed frog

---
+++ The Wide-Mouthed frog/solve.js
@@ -0,0 +1,9 @@
+function mouthSize(animal) {
+    switch (animal) {
+        case "alligator":
+        case "ALLIGATOR":
+            return "small"
+        default:
+            return "wide"
+    }
+}
