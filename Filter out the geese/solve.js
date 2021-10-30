2338ef405ff8b6da29256613769b43e0bb3b05f9
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-10-30 12:45:55 +0300

    solve_2

--- Filter out the geese/solve.js
+++ Filter out the geese/solve.js
@@ -1,6 +1,11 @@
-function gooseFilter(birds) {
+/*function gooseFilter(birds) {
     let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
     return birds.filter(item => geese.indexOf(item) === -1)
+}*/
+
+function gooseFilter(birds) {
+    let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(item => !geese.includes(item))
 }
 
 gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]);
