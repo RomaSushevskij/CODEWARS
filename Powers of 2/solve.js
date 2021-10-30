aa35523a95625d0be6ac484c4e3d142c6163bdb4
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-10-30 13:01:40 +0300

    solve_2

--- Powers of 2/solve.js
+++ Powers of 2/solve.js
@@ -1,7 +1,8 @@
-function powersOfTwo(n) {
+/*function powersOfTwo(n) {
     let answerArr = [];
     for (let i = 0; i <= n; i++) {
         answerArr.push(2 ** i)
     }
     return answerArr
-}
+}*/
+const powersOfTwo = n => Array(n + 1).fill(2).map((e, i) => e**i );
