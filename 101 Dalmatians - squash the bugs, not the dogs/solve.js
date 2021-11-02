9cd41ab383959bc0b23e1bf3ab38e3fc6bd24b5e
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-11-02 17:24:00 +0300

    101 Dalmatians - squash the bugs, not the dogs!

---
+++ 101 Dalmatians - squash the bugs, not the dogs/solve.js
@@ -0,0 +1,8 @@
+function howManyDalmatians(numer) {
+
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+    var respond = numer <= 10 ? dogs[0] : numer <= 50 ? dogs[1] : numer == 101 ? dogs[3] : dogs[2]
+
+    return respond
+}
