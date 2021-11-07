27a67784a83303d2782875802e2747b9baeb868d
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-11-07 13:50:44 +0300

    Grasshopper - Create the rooms

---
+++ Coefficients of the Quadratic Equation/solve.js
@@ -0,0 +1,6 @@
+function quadratic(x1, x2) {
+    let a = 1
+    let b = (x2 * -1) + (x1 * -1)
+    let c = x1 * x2
+    return [a, b, c]
+}
---
+++ Grasshopper - Create the rooms/solve.js
@@ -0,0 +1,6 @@
+// Add rooms here
+var rooms = {
+    firstRoom: {name: "name", description: "description", completed: "completed"},
+    secondRoom: {name: "name", description: "description", completed: "completed"},
+    thirdRood: {name: "name", description: "description", completed: "completed"}
+}
