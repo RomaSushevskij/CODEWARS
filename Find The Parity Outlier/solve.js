95eb80792ee7627a83cd91f9d8c5b94f23a51ac3
RomaSushevskij <roma.sushevskij@yandex.ru>
2021-12-14 11:43:24 +0300

    Find The Parity Outlier

---
+++ Find The Parity Outlier/solve.js
@@ -0,0 +1,40 @@
+function findOutlier(integers) {
+    let countEven = 0;
+    let countOdd = 0;
+    let ans;
+    for (let i = 0; i < integers.length; i += 1) {
+        if (integers[i] % 2 === 0) {
+            countEven += 1;
+            if (countEven >= 2) {
+                break
+            }
+        } else if (integers[i] % 2 !== 0) {
+            countOdd += 1;
+            if (countOdd >= 2) {
+                break
+            }
+        }
+    }
+    if (countEven >= 2) {
+        for (let i = 0; i < integers.length; i += 1) {
+            if (integers[i] % 2 !== 0) {
+                ans = integers[i];
+                break
+            }
+        }
+    } else if (countOdd >= 2) {
+        for (let i = 0; i < integers.length; i += 1) {
+            if (integers[i] % 2 === 0) {
+                ans = integers[i];
+                break
+            }
+        }
+    }
+    return ans
+}
+
+findOutlier([0, 1, 2])
+findOutlier([1, 2, 3])
+findOutlier([2, 6, 8, 10, 3])
+findOutlier([0, 0, 3, 0, 0])
+findOutlier([1, 1, 0, 1, 1])
