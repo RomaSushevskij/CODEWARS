b89d19849168f6fd50a4180e819cd7021ff99cf4
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-01-07 18:23:27 +0300

    Get the Middle Character

---
+++ Get the Middle Character/solve.js
@@ -0,0 +1,8 @@
+function getMiddle(s) {
+    return s.length % 2 === 0 ? s.slice(s.length/2-1, s.length/2+1) : s.slice(s.length/2, s.length/2+1)
+
+}
+getMiddle("test")
+getMiddle("testing")
+getMiddle("middle")
+getMiddle("A")
